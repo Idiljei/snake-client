594284258883e91b796ea994c6feac1220f63872
Idiljei <i.jeilani@gmail.com>
2021-01-13 21:32:34 +0000

    play.js

--- play.js
+++ play.js
@@ -2,23 +2,7 @@
 
 const net = require('net');  
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({  //create connection from net library
-    host: '135.23.222.131',
-    port: 50542
-
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', (data) => {
-    console.log('Server says: ', data); 
-  }); 
-  // this is telling client what to// in this case console.log
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
