0810dc3807363ed18e7bbfcaea10a1b9d3798aec
Idiljei <i.jeilani@gmail.com>
2021-01-14 00:08:30 +0000

    play.js

--- play.js
+++ play.js
@@ -3,26 +3,14 @@
 const net = require('net');  
 
 const { connect } = require('./client');
+const {setupInput} = require('./input')
+
+
 console.log('Connecting ...');
+
 connect();
+setupInput()
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (keydata) => handleUserInput(keydata)) //reads the info you're doin on keyboard 
-  return stdin;
-}
 
-// "data" callback handler for stdin (standard input)
-const handleUserInput = function(key){
-  if (key === '\u0003') { // \u0003 map to ctrl + c input 
-    process.exit()
-  }
-} 
+
 
